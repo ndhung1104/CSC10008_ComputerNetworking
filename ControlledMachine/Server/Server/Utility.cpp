1e43f8cdabe0662a1aacbb225eae03d0b6c6822d
Le Phu Cuong <cuongle6105@gmail.com>
2024-10-22 13:45:06 +0700

    SERVER-CLIENT

---
+++ ControlledMachine/Server/Server/Utility.cpp
@@ -0,0 +1,15 @@
+#include "Utility.h"
+
+void shutdown();
+void reset();
+void keyLogger();
+void listApp();
+void startApp(std::string name);
+void stopApp(std::string name);
+void listService();
+void startService(std::string name);
+void stopService(std::string name);
+void screenShot();
+void copyFile(std::string name);
+void startWebcam();
+void stopWebcam();
