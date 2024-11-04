27b8a7618e22b9a31106bfd67a077f7a671cb5e8
ndhung1104 <ndhung1104@gmail.com>
2024-11-04 20:44:05 +0700

    refactor the code to apply multi threading

--- ControlledMachine/Client/Client/EmailMonitor.cpp
+++ ControlledMachine/Client/Client/EmailMonitor.cpp
@@ -20,6 +20,8 @@ EmailMonitor::EmailMonitor(const std::string& client_id,
     if (!current_token.access_token.empty()) {
         std::cout << "Initial Access Token obtained." << std::endl;
     }
+    else
+        std::cout << "Empty." << std::endl;
 }
 
 void EmailMonitor::sendMessage(std::string message, SOCKET clientSocket)
@@ -34,81 +36,81 @@ void EmailMonitor::sendMessage(std::string message, SOCKET clientSocket)
     }
 }
 
-void EmailMonitor::initializeWhiteList(SOCKET clientSocket)
+void EmailMonitor::initializeWhiteList()
 {
     // Initialize whitelist
     whitelist = {"ndhung23@clc.fitus.edu.vn", "lpcuong23@clc.fitus.edu.vn"};
     
     // Initialize command functions
     commandFunctions = {
-    {"SHUTDOWN", [this, clientSocket](const email& email) { 
+    {"SHUTDOWN", [this](const email& email, const SOCKET& clientSocket) { 
         std::cout << "Executing Command 1\n";
         std::cout << "Email body: " << email.body << "\n";
         sendMessage("SHUTDOWN", clientSocket);
     }},
-    {"RESET", [this, clientSocket](const email& email) { 
+    {"RESET", [this](const email& email, const SOCKET& clientSocket) { 
         std::cout << "Executing Command 1\n";
         std::cout << "Email body: " << email.body << "\n"; 
         sendMessage("RESET", clientSocket);
     }},
-    {"KEYLOGGER", [this, clientSocket](const email& email) { 
+    {"KEYLOGGER", [this](const email& email, const SOCKET& clientSocket) { 
         std::cout << "Executing Command 1\n";
         std::cout << "Email body: " << email.body << "\n"; 
         sendMessage("KEYLOGGER", clientSocket);
         std::this_thread::sleep_for(std::chrono::seconds(10));
         sendMessage("STOP", clientSocket);
     }},
-    {"LISTAPP", [this, clientSocket](const email& email) { 
+    {"LISTAPP", [this](const email& email, const SOCKET& clientSocket) { 
         std::cout << "Executing Command 1\n";
         std::cout << "Email body: " << email.body << "\n"; 
         sendMessage("LISTAPP", clientSocket);
     }},
-    {"STARTAPP", [this, clientSocket](const email& email) { 
+    {"STARTAPP", [this](const email& email, const SOCKET& clientSocket) { 
         std::cout << "Executing Command 1\n";
         std::cout << "Email body: " << email.body << "\n"; 
         sendMessage("STARTAPP", clientSocket);
     }},
-    {"STOPAPP", [this, clientSocket](const email& email) { 
+    {"STOPAPP", [this](const email& email, const SOCKET& clientSocket) { 
         std::cout << "Executing Command 2\n"; 
         std::cout << "Email body: " << email.body << "\n";
         sendMessage("STOPAPP", clientSocket);
     }},
-    {"LISTSERVICES", [this, clientSocket](const email& email) { 
+    {"LISTSERVICES", [this](const email& email, const SOCKET& clientSocket) { 
         std::cout << "Executing Command 2\n"; 
         std::cout << "Email body: " << email.body << "\n";
         sendMessage("LISTSERVICES", clientSocket);
     }},
-    {"STARTSERVICES", [this, clientSocket](const email& email) { 
+    {"STARTSERVICES", [this](const email& email, const SOCKET& clientSocket) { 
         std::cout << "Executing Command 2\n"; 
         std::cout << "Email body: " << email.body << "\n";
         sendMessage("STARTSERVICES", clientSocket);
     }},
-    {"STOPSERVICES", [this, clientSocket](const email& email) { 
+    {"STOPSERVICES", [this](const email& email, const SOCKET& clientSocket) { 
         std::cout << "Executing Command 2\n"; 
         std::cout << "Email body: " << email.body << "\n";
         sendMessage("STOPSERVICES", clientSocket);
     }},
-    {"COPYFILE", [this, clientSocket](const email& email) { 
+    {"COPYFILE", [this](const email& email, const SOCKET& clientSocket) { 
         std::cout << "Executing Command 2\n"; 
         std::cout << "Email body: " << email.body << "\n";
         sendMessage("COPYFILE", clientSocket);
     }},
-    {"REMOVEFILE", [this, clientSocket](const email& email) { 
+    {"REMOVEFILE", [this](const email& email, const SOCKET& clientSocket) { 
         std::cout << "Executing Command 2\n"; 
         std::cout << "Email body: " << email.body << "\n";
         sendMessage("REMOVEFILE", clientSocket);
     }},
-    {"SCREENSHOT", [this, clientSocket](const email& email) { 
+    {"SCREENSHOT", [this](const email& email, const SOCKET& clientSocket) { 
         std::cout << "Executing Command 2\n"; 
         std::cout << "Email body: " << email.body << "\n";
         sendMessage("SCREENSHOT", clientSocket);
     }},
-    {"STARTWEBCAM", [this, clientSocket](const email& email) { 
+    {"STARTWEBCAM", [this](const email& email, const SOCKET& clientSocket) { 
         std::cout << "Executing Command 2\n"; 
         std::cout << "Email body: " << email.body << "\n";
         sendMessage("STARTWEBCAM", clientSocket);
     }},
-    {"STOPWEBCAM", [this, clientSocket](const email& email) { 
+    {"STOPWEBCAM", [this](const email& email, const SOCKET& clientSocket) { 
         std::cout << "Executing Command 2\n"; 
         std::cout << "Email body: " << email.body << "\n";
         sendMessage("STOPWEBCAM", clientSocket);
@@ -125,115 +127,153 @@ void EmailMonitor::refreshTokenIfNeeded() {
     }
 }
 
-void EmailMonitor::start() {
-    std::cout << "======= W11 Sockets =======\n";
-    std::cout << "========= SERVER ==========\n";
-    std::cout << "=== Step 1 - Set up DLL ===\n\n";
+void EmailMonitor::start(const std::vector<SOCKET>& socketVector) {
+    refreshTokenIfNeeded();
+    gmail.getEmailList(current_token.access_token);
 
-    SOCKET clientSocket, acceptSocket;
-    int port = 55555;
-    WSADATA wsaData;
-    int wsaerr;
-    WORD wVersionRequested = MAKEWORD(2, 2);
-    wsaerr = WSAStartup(wVersionRequested, &wsaData);
-    if (wsaerr != 0) {
-        std::cout << "The Winsock dll not found!" << std::endl;
-        return;
+    if (gmail.receivedEmail.size() > 0)
+    {
+        processEmails(gmail.receivedEmail, socketVector);
     }
-    else {
-        std::cout << "The Winsock dll found!" << std::endl;
-        std::cout << "The status: " << wsaData.szSystemStatus << std::endl;
-    }
+    
+}
 
-    std::cout << "\n=== Step 2 - Set up Server Socket ===\n\n";
+    // std::cout << "======= W11 Sockets =======\n";
+    // std::cout << "========= SERVER ==========\n";
+    // std::cout << "=== Step 1 - Set up DLL ===\n\n";
 
-    clientSocket = INVALID_SOCKET;
-    clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (clientSocket == INVALID_SOCKET) {
-        std::cout << "Error at socket(): " << WSAGetLastError() << std::endl;
-        WSACleanup();
-        return;
-    }
-    else std::cout << "socket() is OK!" << std::endl;
+    // SOCKET clientSocket, acceptSocket;
+    // int port = 55555;
+    // WSADATA wsaData;
+    // int wsaerr;
+    // WORD wVersionRequested = MAKEWORD(2, 2);
+    // wsaerr = WSAStartup(wVersionRequested, &wsaData);
+    // if (wsaerr != 0) {
+    //     std::cout << "The Winsock dll not found!" << std::endl;
+    //     return;
+    // }
+    // else {
+    //     std::cout << "The Winsock dll found!" << std::endl;
+    //     std::cout << "The status: " << wsaData.szSystemStatus << std::endl;
+    // }
 
-    std::cout << "\n=== Step 3 - Bind Socket ===\n\n";
+    // std::cout << "\n=== Step 2 - Set up Server Socket ===\n\n";
 
-    sockaddr_in service;
-    service.sin_family = AF_INET;
-    InetPton(AF_INET, "10.123.0.185", &service.sin_addr.s_addr);
-    service.sin_port = htons(port);
-    if (bind(clientSocket, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR) {
-        std::cout << "bind() failed: " << WSAGetLastError() << std::endl;
-        closesocket(clientSocket);
-        WSACleanup();
-        return;
-    }
-    else std::cout << "bind() is OK!" << std::endl;
+    // clientSocket = INVALID_SOCKET;
+    // clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    // if (clientSocket == INVALID_SOCKET) {
+    //     std::cout << "Error at socket(): " << WSAGetLastError() << std::endl;
+    //     WSACleanup();
+    //     return;
+    // }
+    // else std::cout << "socket() is OK!" << std::endl;
 
-    std::cout << "\n=== Step 4 - Initiate L0isten ===\n\n";
+    // std::cout << "\n=== Step 3 - Bind Socket ===\n\n";
 
-    if (listen(clientSocket, 1) == SOCKET_ERROR) {
-        std::cout << "listen(): Error listening on socket " << WSAGetLastError() << std::endl;
-        WSACleanup();
-        return;
-    }
-    else {
-        std::cout << "listen() is OK, I'm waiting for connections..." << std::endl;
-    }
+    // sockaddr_in service;
+    // service.sin_family = AF_INET;
+    // InetPton(AF_INET, "10.123.0.185", &service.sin_addr.s_addr);
+    // service.sin_port = htons(port);
+    // if (bind(clientSocket, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR) {
+    //     std::cout << "bind() failed: " << WSAGetLastError() << std::endl;
+    //     closesocket(clientSocket);
+    //     WSACleanup();
+    //     return;
+    // }
+    // else std::cout << "bind() is OK!" << std::endl;
 
-    while (true) {
-        std::cout << "\n=== Step 5 - Accept Connection from Client ===\n\n";
+    // std::cout << "\n=== Step 4 - Initiate L0isten ===\n\n";
 
-        acceptSocket = accept(clientSocket, NULL, NULL);
-        if (acceptSocket == INVALID_SOCKET) {
-            std::cout << "accept failed: " << WSAGetLastError() << std::endl;
-            WSACleanup();
-            return;
-        }
-        std::cout << "Accepted connection" << std::endl;
+    // if (listen(clientSocket, 1) == SOCKET_ERROR) {
+    //     std::cout << "listen(): Error listening on socket " << WSAGetLastError() << std::endl;
+    //     WSACleanup();
+    //     return;
+    // }
+    // else {
+    //     std::cout << "listen() is OK, I'm waiting for connections..." << std::endl;
+    // }
 
-        std::cout << "\n=== Step 6 - Receive Message from Client ===\n\n";
+    // while (true) {
+    //     std::cout << "\n=== Step 5 - Accept Connection from Client ===\n\n";
 
-        char buffer[bufferSize];
-        int byteCount;
-        initializeWhiteList(acceptSocket);
-        while (true) {
-            // Nhận tin nhắn từ client
-            sendMessage("Sending", acceptSocket);
-            refreshTokenIfNeeded();
-            gmail.getEmailList(current_token.access_token);
+    //     acceptSocket = accept(clientSocket, NULL, NULL);
+    //     if (acceptSocket == INVALID_SOCKET) {
+    //         std::cout << "accept failed: " << WSAGetLastError() << std::endl;
+    //         WSACleanup();
+    //         return;
+    //     }
+    //     std::cout << "Accepted connection" << std::endl;
 
-            if (gmail.receivedEmail.size() > 0)
-            {
-                std::cout << "New " << gmail.receivedEmail.size() << " mail!\n";
-                processEmails(gmail.receivedEmail);
-            }
-            else 
-                std::cout << "No new mail!\n";
-            std::this_thread::sleep_for(std::chrono::seconds(10));
-            }
+    //     std::cout << "\n=== Step 6 - Receive Message from Client ===\n\n";
 
-            closesocket(acceptSocket); // Đóng kết nối với client
-    }
+    //     char buffer[bufferSize];
+    //     int byteCount;
+    //     initializeWhiteList(acceptSocket);
+    //     while (true) {
+    //         // Nhận tin nhắn từ client
+    //         sendMessage("Sending", acceptSocket);
+    //         refreshTokenIfNeeded();
+    //         gmail.getEmailList(current_token.access_token);
 
-    closesocket(clientSocket);
-    WSACleanup();
+    //         if (gmail.receivedEmail.size() > 0)
+    //         {
+    //             std::cout << "New " << gmail.receivedEmail.size() << " mail!\n";
+    //             processEmails(gmail.receivedEmail);
+    //         }
+    //         else 
+    //             std::cout << "No new mail!\n";
+    //         std::this_thread::sleep_for(std::chrono::seconds(10));
+    //         }
+
+    //         closesocket(acceptSocket); // Đóng kết nối với client
+    // }
+
+    // closesocket(clientSocket);
+    // WSACleanup();
 
     
     // gmail.sendEmail(current_token.access_token, "ndhung23@clc.fitus.edu.vn", "ABCXYZ", "lalalalala");
+// }
+
+bool EmailMonitor::findSocketByIP(const std::string& ipAddress, const std::vector<SOCKET>& socketVector, SOCKET& foundSocket) {
+    struct sockaddr_in addr;
+    socklen_t addrLen = sizeof(addr);
+
+    for (const SOCKET& socket : socketVector) {
+        // Get the peer (remote) address of the socket
+        if (getpeername(socket, (struct sockaddr*)&addr, &addrLen) == 0) {
+            char socketIP[INET_ADDRSTRLEN];
+
+            // Convert the IP address from binary to text form
+            inet_ntop(AF_INET, &addr.sin_addr, socketIP, INET_ADDRSTRLEN);
+
+            // Compare the socket's IP address with the target IP address
+            if (ipAddress == socketIP) {
+                foundSocket = socket; // Assign the matching socket to foundSocket
+                return true;           // Socket with matching IP found
+            }
+        }
+    }
+
+    return false; // No matching socket found
 }
 
 
-
-void EmailMonitor::processEmails(std::vector<email>& receivedEmails)
+void EmailMonitor::processEmails(std::vector<email>& receivedEmails, const std::vector<SOCKET>& socketVector)
 {
     auto it = receivedEmails.begin();
     while (it != receivedEmails.end()) {
         if (whitelist.count(it->sender)) {
             auto command = commandFunctions.find(it->subject);
             if (command != commandFunctions.end()) {
-                std::cout << "Processing email from: " << it->sender << " with command: " << it->subject << "\n";
-                command->second(*it); // Execute the function associated with the subject command
+                //std::cout << "Processing email from: " << it->sender << " with command: " << it->subject << "\n";
+                std::istringstream stream((*it).body);
+                std::string firstLine;
+                std::getline(stream, firstLine);
+
+                SOCKET targetSocket;
+                if (findSocketByIP(firstLine, socketVector, targetSocket))
+                    command->second(*it, targetSocket); // Execute the function associated with the subject command
             }
         }
         it = receivedEmails.erase(it); // Remove the processed email and update the iterator
